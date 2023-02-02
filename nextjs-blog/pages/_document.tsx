00709eeb1e8dd3615bebb2ca51fe88983d02f50d
Bas de Ruiter <deruiterb@gmail.com>
2023-02-02 14:00:03 +0100

    Assets, Metadata, and CSS - lesson 4: Metada
    Added tailwind to play around with it

---
+++ nextjs-blog/pages/_document.tsx
@@ -0,0 +1,13 @@
+import {Head, Html, Main, NextScript} from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="nl">
+      <Head />
+      <body className="bg-teal-100">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
